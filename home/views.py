e79d78ac81f105e33f0737e43e14361963715582
Sohaib_LangriaL <52604138+LaaL-Khan-LangriaL@users.noreply.github.com>
2023-05-26 01:58:39 +0500

    meta tags keywords removed from header page

--- home/views.py
+++ home/views.py
@@ -12,8 +12,7 @@ from django.contrib import messages
 
 def sohaib_Home(request):
     social_media = SocialMedia.objects.all()
-    latest_keywords = userBlog.objects.order_by('-sNo').values_list('tags', flat=True)[:3]
-    context = {'social_media': social_media, 'latest_keywords': latest_keywords}
+    context = {'social_media': social_media}
     return render(request, 'main_Home.html', context)
 
 
@@ -26,10 +25,8 @@ def Portfolio(request):
     portfolio = Portfolios.objects.all()
     social_media = SocialMedia.objects.all()
     blog = userBlog.objects.all().order_by('-sNo')[:2]
-    latest_keywords = userBlog.objects.order_by('-sNo').values_list('tags', flat=True)[:3]
     data = {'about': about, 'experience': experience, 'education': education, 'lang_skill': languages_skills,
-            'portfolio': portfolio, 'Recommendation': Recommendation, 'social_media': social_media, 'blog': blog,
-            'latest_keywords': latest_keywords}
+            'portfolio': portfolio, 'Recommendation': Recommendation, 'social_media': social_media, 'blog': blog}
     return render(request, 'portfolio.html', data)
 
 
@@ -42,9 +39,8 @@ def blog_list(request):
     SMDT = SocialMedia.objects.all()
     RCPST = userBlog.objects.all().order_by('-sNo')[10:16]
     footer_recent = userBlog.objects.all().order_by('-sNo')[:2]
-    latest_keywords = userBlog.objects.order_by('-sNo').values_list('tags', flat=True)[:3]
     context = {'BLOGDATA': BLOGDATAFINAL, 'lastPage': totalPages, 'pageList': [n + 1 for n in range(totalPages)],
-               'RCPST': RCPST, 'SMDT': SMDT, 'footer_recent': footer_recent, 'latest_keywords': latest_keywords}
+               'RCPST': RCPST, 'SMDT': SMDT, 'footer_recent': footer_recent}
     return render(request, 'blog_list.html', context)
 
 
@@ -59,9 +55,8 @@ def search_blog(request):
         SMDT = SocialMedia.objects.all()
         RCPST = userBlog.objects.all().order_by('-sNo')[10:16]
         footer_recent = userBlog.objects.all().order_by('-sNo')[:2]
-        latest_keywords = userBlog.objects.order_by('-sNo').values_list('tags', flat=True)[:3]
         context = {'BLOGDATA': BLOGDATAFINAL, 'lastPage': totalPages, 'pageList': [n + 1 for n in range(totalPages)],
-                   'RCPST': RCPST, 'SMDT': SMDT, 'footer_recent': footer_recent, 'latest_keywords': latest_keywords}
+                   'RCPST': RCPST, 'SMDT': SMDT, 'footer_recent': footer_recent}
         return render(request, 'blog_list.html', context)
 
 
@@ -96,9 +91,8 @@ def PIAIC_Attachs(request):
     totalPages = Final_Attachments.paginator.num_pages
     SMDT = SocialMedia.objects.all()
     RCPST = userBlog.objects.all().order_by('-sNo')[:2]
-    latest_keywords = userBlog.objects.order_by('-sNo').values_list('tags', flat=True)[:3]
     context = {'Final_Attachments': Final_Attachments, 'lastPage': totalPages, 'pageList': [n + 1 for n in range(totalPages)],
-               'RCPST': RCPST, 'SMDT': SMDT, 'latest_keywords': latest_keywords}
+               'RCPST': RCPST, 'SMDT': SMDT}
     return render(request, 'PIAIC.html', context)
 
 
@@ -122,9 +116,8 @@ def PIAIC_NOTIFI(request):
     totalPages = Final_Attachments.paginator.num_pages
     SMDT = SocialMedia.objects.all()
     RCPST = userBlog.objects.all().order_by('-sNo')[:2]
-    latest_keywords = userBlog.objects.order_by('-sNo').values_list('tags', flat=True)[:3]
     context = {'Final_Attachments': Final_Attachments, 'lastPage': totalPages, 'pageList': [n + 1 for n in range(totalPages)],
-               'RCPST': RCPST, 'SMDT': SMDT, 'latest_keywords': latest_keywords}
+               'RCPST': RCPST, 'SMDT': SMDT}
     return render(request, 'piaic_notifications.html', context)
 
 
@@ -155,9 +148,7 @@ def Detail_Record(request, sNo, type):
         SMDT = SocialMedia.objects.all()
         RCPST = userBlog.objects.all().order_by('-sNo')[10:16]
         footer_recent = userBlog.objects.all().order_by('-sNo')[:2]
-        latest_keywords = userBlog.objects.order_by('-sNo').values_list('tags', flat=True)[:3]
-        context = {'rdPost': rdPost, 'RCPST': RCPST, 'SMDT': SMDT, 'coments': coments, 'footer_recent': footer_recent,
-                   'latest_keywords': latest_keywords}
+        context = {'rdPost': rdPost, 'RCPST': RCPST, 'SMDT': SMDT, 'coments': coments, 'footer_recent': footer_recent}
         return render(request, 'read_post.html', context)
 
     if type == 'Attachment_Detail':
@@ -167,9 +158,8 @@ def Detail_Record(request, sNo, type):
         piaic_query = PIAIC_Review.objects.filter(post__in=readAttachment).order_by('-sNo')
         SMDT = SocialMedia.objects.all()
         RCPST = userBlog.objects.all().order_by('-sNo')[:2]
-        latest_keywords = userBlog.objects.order_by('-sNo').values_list('tags', flat=True)[:3]
         context = {'readAttachment': readAttachment, 'Attachments': Attachments, 'piaic_query': piaic_query,
-                   'ICONS': ICONS, 'RCPST': RCPST, 'SMDT': SMDT, 'latest_keywords': latest_keywords}
+                   'ICONS': ICONS, 'RCPST': RCPST, 'SMDT': SMDT}
         return render(request, 'Read_Attachment.html', context)
 
     if type == 'Notification_Detail':
@@ -177,7 +167,6 @@ def Detail_Record(request, sNo, type):
         piaic_query = PIAIC_NOTIFI_Review.objects.filter(post__in=readAttachment).order_by('-sNo')
         SMDT = SocialMedia.objects.all()
         RCPST = userBlog.objects.all().order_by('-sNo')[:2]
-        latest_keywords = userBlog.objects.order_by('-sNo').values_list('tags', flat=True)[:3]
         context = {'readAttachment': readAttachment, 'piaic_query': piaic_query,
-                   'RCPST': RCPST, 'SMDT': SMDT, 'latest_keywords': latest_keywords}
+                   'RCPST': RCPST, 'SMDT': SMDT}
         return render(request, 'Read_PIAIC_Notifications.html', context)
