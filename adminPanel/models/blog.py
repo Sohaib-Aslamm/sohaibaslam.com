97b87c6c16054e1efdb88286886d44a65f2891dc
Sohaib_LangriaL <52604138+LaaL-Khan-LangriaL@users.noreply.github.com>
2023-06-04 13:39:55 +0500

    slug added for blog

--- adminPanel/models/blog.py
+++ adminPanel/models/blog.py
@@ -1,11 +1,13 @@
 from django.db import models
 import django.utils.timezone
 from ckeditor.fields import RichTextField
+from django.utils.text import slugify
 
 
 class userBlog(models.Model):
     sNo = models.AutoField(primary_key=True)
     title = models.TextField(default="")
+    slug = models.SlugField(max_length=200, unique=True, default=slugify(title))
     heading = models.TextField(default="")
     tags = models.TextField(default="")
     quote = models.TextField(default="")
@@ -13,6 +15,10 @@ class userBlog(models.Model):
     description = RichTextField(default="")
     Icon = models.FileField(upload_to='Blog/Icons', default="")
     created_at = models.DateTimeField(default=django.utils.timezone.now())
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
 
 class blog_Review(models.Model):
